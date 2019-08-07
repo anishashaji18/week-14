968fa7d1fbf2c862ea153c43937ebfb2f9f5d9c4
Anisha Shaji <753943@student.tor.loc>
2019-08-07 17:02:56 -0400

    5pm

--- project.js
+++ project.js
@@ -19,9 +19,27 @@ site.listen(3000);      */
 
 var express = require('express');
 var app = express()
+const bodyparser  =  require('body-parser');
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+
 
 app.get('/',function(req,res){
     res.send('helooo there')
+})
+app.post('/',function(req,res){
+
+    res.send('<h1>post send</h1>')
+})
+app.put('/',function(req,res){
+
+    res.send('this is put')
+})
+app.delete('/',function(req,res){
+
+    res.send('delete')
 })
 
 app.listen(3000)
