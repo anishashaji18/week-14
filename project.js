09407dfea11fbd00a97fddbbafcb8b4a81e809a6
Anisha Shaji <753943@student.tor.loc>
2019-08-07 16:14:00 -0400

    2nd commit

---
+++ project.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const data ={
+    firstname:'anisha',
+    lastname: 'shaji'
+}
+const site = http.createServer(function (req,res){
+             console.log('hello world');
+console.log(req.headers);
+console.log(req.url);
+     res.setHeader('Content-Type','application/json');
+         
+     res.write(JSON.stringify(data));
+           //  res.write('<h1>helloooooo</h1>');
+res.end();
+
+
+
+});
+
+site.listen(3000);
