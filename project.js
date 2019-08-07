76c8823e4e074ae42ab73a4b59b28809b899b2f3
Anisha Shaji <753943@student.tor.loc>
2019-08-07 16:20:14 -0400

    3rd

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 const data ={
     firstname:'anisha',
     lastname: 'shaji'
@@ -7,14 +7,21 @@ const site = http.createServer(function (req,res){
              console.log('hello world');
 console.log(req.headers);
 console.log(req.url);
-     res.setHeader('Content-Type','application/json');
-         
+     res.setHeader('Content-Type','application/json');   
      res.write(JSON.stringify(data));
+           //res.setHeader('content-type','text/html');
            //  res.write('<h1>helloooooo</h1>');
 res.end();
-
-
-
 });
 
-site.listen(3000);
+site.listen(3000);      */
+
+
+var express = require('express');
+var app = express()
+
+app.get('/',function(req,res){
+    res.send('helooo there')
+})
+
+app.listen(3000)
